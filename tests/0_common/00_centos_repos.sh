127ce7ac25ac53e488c6611e8f2ee4e0d6d7e89b
Houman Ekrami <houmanekrami@gmail.com>
2022-12-14 15:50:27 -0800

    Delete 00_centos_repos.sh

--- tests/0_common/00_centos_repos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$centos_ver" -ge "8" ]; then
-  t_Log "python not installed by default on .el8. SKIP"
-  exit 0
-else
-   python tests/0_common/000_centos_default_repos.py
-fi
-
