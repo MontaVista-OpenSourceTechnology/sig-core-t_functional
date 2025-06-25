f3c4e6ad0015bf3627cf4149806cc88955c931aa
bbotti <bbotti@mvista.com>
2025-06-25 14:40:53 -0700

    Fixed sytax error in 0_install_gzip.sh

--- tests/p_gzip/0_install_gzip.sh
+++ tests/p_gzip/0_install_gzip.sh
@@ -5,7 +5,7 @@ t_Log "Running $0 - attempting to install gzip."
 t_InstallPackage gzip zip diffutils less util-linux-ng expect
 
 # Skip if after CentOS 8
-if [ "$centos_ver" -gt "8" ]; then
+if [ "$centos_ver" -gt "8" ]
 then
   t_Log "Ncompress unavailable post-c8 => SKIP"
 else
